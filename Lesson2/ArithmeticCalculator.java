3a80ea9813e7e216741617129abbe4c29ce24a6b
Trần Kiên Cường <trankiencuong30072003@gmail.com>
2024-11-12 18:14:20 +0700

    Learned variable concepts in Lesson2

---
+++ Lesson2/ArithmeticCalculator.java
@@ -0,0 +1,8 @@
+package Lesson2;
+
+public class ArithmeticCalculator {
+    public static void main(String[] args) {
+        int test = 5 + 9*3 - 10/2;
+        System.out.println("Check test: " + test);
+    }
+}
---
+++ Lesson2/SumCalculator.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        int a = 6;
+        int b= 9;
+        int c = a + b;
+        System.out.println("Sum: " + c);
+    }
+}
